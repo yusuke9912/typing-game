811768c7600e7f1ba454e05eeeb12977d3ae0d7b
test <yusuke.1202.n@gmail.com>
2022-06-21 23:12:01 +0900

    [add]indexページの追加

---
+++ resources/js/components/IndexComponent.vue
@@ -0,0 +1,48 @@
+<template>
+<div>
+<header>
+            <h1>TypingGame</h1>
+        </header>
+        <div class="container">
+            <p id="count"></p>
+        <p id="time"></p>
+        <h1>タイピングスキルを競おう!</h1>
+        <h2>
+            <p id="japanese"></p>
+            <p id="romaji"></p>
+        </h2>
+        <table id="displayScore">
+            <tr>
+                <td class="label">スコア:</td>
+                <td class="scoreArea"></td>
+            </tr>
+            <tr>
+                <td class="label">正タイプ率:</td>
+                <td class="scoreArea"></td>
+            </tr>
+            <tr>
+                <td class="label">平均タイプ数:</td>
+                <td class="scoreArea"></td>
+            </tr>
+        </table>
+
+        <input type="button" id="start_button" value="スタート">
+        <form id="limitForm" action="ranking.php" method="post">
+            <p><label for="selectLimit">制限時間を選択</label></p>
+            <select name="limit" id="selectLimit">
+                <option value="30">30秒</option>
+                <option value="60" selected>60秒</option>
+                <option value="90">90秒</option>
+            </select>
+        </form>
+        </div>
+        </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link href="{{ mix('css/app.css') }}" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <div id="app">
+            <index-component></index-component>
+        </div><!-- #app -->
+        <script src="{{ mix('js/app.js') }}"></script>
+    </body>
+</html>
